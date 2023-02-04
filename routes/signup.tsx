6917c97fa19ce5b4ff9595668d1e7ad019cae63c
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-02-04 12:14:45 -0800

    feat: fr fr finished signup look

--- routes/signup.tsx
+++ routes/signup.tsx
@@ -26,15 +26,16 @@ export default function Signup() {
             <h1 class="text-center text-gray-600">Sign up to Allo to ensure your  <br /> childrens' financial education</h1>
           </div>
           <div class="flex flex-col gap-4">
-            <input class="px-4 py-2 border rounded w-72" placeholder="Email Address"/>
-            <input class="px-4 py-2 border rounded w-72" placeholder="Password"/>
+            <input type="email" class="px-4 py-2 border-2 rounded w-72 outline-none focus:border-[#6c63ff] invalid:border-pink-500" placeholder="Email Address"/>
+            <input type="password" class="px-4 py-2 border-2 focus:border-[#6c63ff] rounded w-72 outline-none" placeholder="Password"/>
+            <button href="/login/google" class="px-4 py-2 bg-gray-100 hover:bg-[#6c63ff] hover:text-white transition-all rounded flex gap-4 justify-center">Continue</button>
             <div class="text-gray-500 flex items-center justify-between">
               <div class="border h-0 w-32" />
               or
               <div class="border h-0 w-32" />
             </div>
-            <a class="px-4 py-2 bg-gray-100 rounded flex gap-4 justify-center"><img src="/icons/google.svg"/><span>Continue with Google</span></a>
-            <a class="px-4 py-2 bg-gray-100 rounded flex gap-4 justify-center"><img src="/icons/microsoft.svg"/><span>Continue with Microsoft</span></a>
+            <a href="/login/google" class="px-4 py-2 bg-gray-100 hover:bg-gray-300 rounded flex gap-4 justify-center transition-all"><img src="/icons/google.svg"/><span>Continue with Google</span></a>
+            <a href="/login/microsoft" class="px-4 py-2 bg-gray-100 hover:bg-gray-300 rounded flex gap-4 justify-center transition-all"><img src="/icons/microsoft.svg"/><span>Continue with Microsoft</span></a>
           </div>
         </div>
       </div>
